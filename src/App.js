9c21ca16a2ce1a823101e3b3a28024fcab9f70c7
Rohit <rdg922@gmail.com>
2020-09-12 12:41:05 -0400

    Todo App working

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+// import { Container, Row, Col, Button, Alert, Breadcrumb, Card, Form } from 'react-bootstrap';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+//importing Components
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
+import SettingsCog from "./components/SettingsCog";
+
 function App() {
+
+  // States
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState("all");
+  const [filteredTodos, setFilteredTodos] = useState([]);
+
+  // Use Effect
+
+  useEffect(() => {
+    getLocalTodos();
+  }, []);
+
+  useEffect(() => {
+    filterHandler()
+    saveLocalTodos()
+  }, [todos, status])
+
+  // Functions
+  const filterHandler = () => {
+    switch (status) {
+      case 'completed':
+        setFilteredTodos(todos.filter((todo) => todo.completed === true));
+        break;
+      case 'uncompleted':
+        setFilteredTodos(todos.filter((todo) => todo.completed === false));
+        break;
+      default:
+        setFilteredTodos(todos);
+        break;
+    }
+  }
+
+  // Save to Local
+  const saveLocalTodos = () => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }
+
+  const getLocalTodos = () => {
+    if (localStorage.getItem("todos") === null) {
+      localStorage.setItem("todos", JSON.stringify([]));
+    } else {
+      let todosLocal = (JSON.parse(localStorage.getItem('todos')))
+      setTodos(todosLocal)
+    }
+  }
+
+  // Run ONCE when app starts
+  // getLocalTodos()
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Rohit's Todo List</h1>
       </header>
+      <Form todos={todos} setTodos={setTodos} inputText={inputText} setInputText={setInputText} setStatus={setStatus} />
+      <TodoList todos={todos} setTodos={setTodos} filteredTodos={filteredTodos} />
+      <SettingsCog />
     </div>
   );
 }
---
+++ src/components/SettingsCog.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SettingsCog = () => {
+    return(
+        <footer>
+            <i className="fas fa-google fa-4x">Sign in with Gmail</i>
+        </footer>
+    );
+}
+
+export default SettingsCog
